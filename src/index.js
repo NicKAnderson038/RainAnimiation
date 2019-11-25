ee77a969ff9a492805ccf5764a42429b439fccd2
NicKAnderson038 <andersonnick038@gmail.com>
2019-11-24 20:34:56 -0800

    js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-// number of drops created.
-const nbDrop = 858
-
 // function to generate a random number range.
 const randRange = (minNum, maxNum) =>
   Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum
 
 // function to generate drops
-function createRain() {
+function createRain(nbDrop) {
   for (i = 1; i < nbDrop; i++) {
-    const dropLeft = randRange(0, 1600)
-    const dropTop = randRange(-1000, 1400)
-
     $('.rain').append('<div class="drop" id="drop' + i + '"></div>')
-    $('#drop' + i).css('left', dropLeft)
-    $('#drop' + i).css('top', dropTop)
+    $('#drop' + i).css('left', randRange(0, 1600))
+    $('#drop' + i).css('top', randRange(-1000, 1400))
   }
 }
-// Make it rain
-createRain()
+// Make it rain & number of drops
+createRain(858)
